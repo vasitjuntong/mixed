addb3cecc17ff8fb92f4115835b1aae12ccd4233
Vasit Juntong <N_vasit@hotmail.com>
2016-01-03 18:15:25 +0700

    ลบ Code ที่ไม่ได้ใช้

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Location;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\LocationCreateRequest;
 use App\Http\Requests\LocationUpdateRequest;
 
@@ -18,22 +17,6 @@ class LocationController extends Controller
         return view('locations.index', compact('locations'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(LocationCreateRequest $request)
     {
         Location::create($request->all());
@@ -46,23 +29,6 @@ class LocationController extends Controller
         return redirect('/locations');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $location = Location::find($id);
@@ -70,13 +36,6 @@ class LocationController extends Controller
         return view('locations.edit', compact('location'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(LocationUpdateRequest $request, $id)
     {
         $location = new Location($request->all());
@@ -93,35 +52,27 @@ class LocationController extends Controller
         return redirect('/locations');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Request $request, $id)
     {
-
         $response = Location::deleteByCondition($id);
 
-
-        if($request->ajax())
+        if ($request->ajax()) {
             return $response;
+        }
 
-        if($response['status']){
+        if ($response['status']) {
             flash()
                 ->success(
                     trans('location.label.name'),
                     $response['message']
                 );
-        }else{
+        } else {
             flash()
                 ->success(
                     trans('location.label.name'),
                     $response['message']
                 );
         }
-
 
         return redirect('/locations');
 

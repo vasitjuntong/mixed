5797d26e958eb034996b2e7cd6f0247d0080fd7e
Vasit Juntong <n_vasit@hotmail.com>
2016-02-03 23:35:13 +0700

    feed receive feed requisition
    count product, receive, requisition

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\Receive;
+use App\Requesition;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,6 +14,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $productCount = Product::count();
+        $receiveCount = Receive::count();
+        $requisitionCount = Requesition::count();
+
+        $receives = Receive::latest()->limit(5)->get();
+        $requisitions = Requesition::latest()->limit(5)->get();
+
+        return view('home.index', compact('productCount',
+            'receiveCount',
+            'requisitionCount',
+            'receives',
+            'requisitions'
+        ));
     }
 }
